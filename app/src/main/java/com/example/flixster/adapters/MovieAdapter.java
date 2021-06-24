f91b67c062f426edc9986979efaa9df3849fd915
alishadnaidu <alishanaidu@fb.com>
2021-06-23 21:50:17 -0700

    Initial commit

---
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -0,0 +1,124 @@
+package com.example.flixster.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.flixster.MovieDetailsActivity;
+import com.example.flixster.R;
+import com.example.flixster.models.Movie;
+
+import org.jetbrains.annotations.NotNull;
+import org.parceler.Parcels;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    //inflates a layout from XML, returns a holder
+    @NonNull
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter", "onCreateViewHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
+    }
+
+    //populates data into the item through holder
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder" + position);
+        //get the movie at the position that is passed in
+        Movie movie = movies.get(position);
+        //bind the movie data into the ViewHolder
+        holder.bind(movie);
+
+    }
+
+    //return total count of items in list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+            itemView.setOnClickListener(this);
+        }
+
+        //showing movie details when user clicks on a row
+        @Override
+        public void onClick(View v) {
+            //get item position
+            int position = getAdapterPosition();
+            //ensure that position exists in view
+            if (position != RecyclerView.NO_POSITION) {
+                //get the movie at the specified position
+                Movie movie = movies.get(position);
+                //create intent for the new activity
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                //pass the movie as an extra serialized via Parcels.wrap()
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                //show activity
+                context.startActivity(intent);
+            }
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            String imageUrl;
+            String placeholderUrl;
+            //if phone in landscape: set imageUrl to backdrop image
+            //else (phone in portrait): set imageUrl to poster image
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageUrl = movie.getBackdropPath();
+                Glide.with(context)
+                        .load("http://via.placeholder.com/300.png")
+                        .placeholder(R.drawable.flicks_backdrop_placeholder)
+                        .error(R.drawable.flicks_backdrop_placeholder)
+                        .into(ivPoster);
+            }
+            else {
+                imageUrl = movie.getPosterPath();
+                Glide.with(context)
+                        .load("http://via.placeholder.com/300.png")
+                        .placeholder(R.drawable.flicks_movie_placeholder)
+                        .error(R.drawable.flicks_backdrop_placeholder)
+                        .into(ivPoster);
+            }
+
+            Glide.with(context).load(imageUrl).into(ivPoster);
+
+        }
+    }
+}
